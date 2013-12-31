092448ed8b3128dba273eca436dbc1d9db8f78f9
sandeepk01 <sandeep.bhat.vns@gmail.com>
2013-12-31 16:42:28 +0530

    Initial Import

---
+++ Sandeep/FlightSearchDemo/FlightSearch/WebContent/resources/scripts/flightData.js
@@ -0,0 +1,173 @@
+function flightData() {
+}
+flightData.json = [ {
+	"flightId" : "G8-115",
+	"airline" : "Go Air",
+	"source" : "New Delhi",
+	"sourceCode" : "DEL",
+	"destination" : "Bangalore",
+	"destCode" : "BLR",
+	"depTime" : "9:40",
+	"arrTime" : "12:10",
+	"cost" : "4085"
+}, {
+	"flightId" : "G8-117",
+	"airline" : "Go Air",
+	"source" : "New Delhi",
+	"sourceCode" : "DEL",
+	"destination" : "Bangalore",
+	"destCode" : "BLR",
+	"depTime" : "17:30",
+	"arrTime" : "20:10",
+	"cost" : "4696"
+}, {
+	"flightId" : "SG-501",
+	"airline" : "Spicejet",
+	"source" : "New Delhi",
+	"sourceCode" : "DEL",
+	"destination" : "Bangalore",
+	"destCode" : "BLR",
+	"depTime" : "6:05",
+	"arrTime" : "8:55",
+	"cost" : "5664"
+}, {
+	"flightId" : "6E-115",
+	"airline" : "Indigo",
+	"source" : "New Delhi",
+	"sourceCode" : "DEL",
+	"destination" : "Bangalore",
+	"destCode" : "BLR",
+	"depTime" : "8:05",
+	"arrTime" : "10:45",
+	"cost" : "5697"
+}, {
+	"flightId" : "9W-7125",
+	"airline" : "Jet Airways",
+	"source" : "New Delhi",
+	"sourceCode" : "DEL",
+	"destination" : "Bangalore",
+	"destCode" : "BLR",
+	"depTime" : "9:35",
+	"arrTime" : "12:20",
+	"cost" : "7561"
+}, {
+	"flightId" : "AI-506",
+	"airline" : "Air India",
+	"source" : "New Delhi",
+	"sourceCode" : "DEL",
+	"destination" : "Bangalore",
+	"destCode" : "BLR",
+	"depTime" : "9:45",
+	"arrTime" : "12:30",
+	"cost" : "10064"
+}, {
+	"flightId" : "AI-403",
+	"airline" : "Air India",
+	"source" : "New Delhi",
+	"sourceCode" : "DEL",
+	"destination" : "Bangalore",
+	"destCode" : "BLR",
+	"depTime" : "16:05",
+	"arrTime" : "19:25",
+	"cost" : "5615"
+}, {
+	"flightId" : "S2-4235",
+	"airline" : "Jet Konnect",
+	"source" : "New Delhi",
+	"sourceCode" : "DEL",
+	"destination" : "Bangalore",
+	"destCode" : "BLR",
+	"depTime" : "19:15",
+	"arrTime" : "21:55",
+	"cost" : "4565"
+}, {
+	"flightId" : "SG-502",
+	"airline" : "Spicejet",
+	"source" : "Bangalore",
+	"sourceCode" : "BLR",
+	"destination" : "New Delhi",
+	"destCode" : "DEL",
+	"depTime" : "6:35",
+	"arrTime" : "9:25",
+	"cost" : "5665"
+}, {
+	"flightId" : "G8-116",
+	"airline" : "Go Air",
+	"source" : "Bangalore",
+	"sourceCode" : "BLR",
+	"destination" : "New Delhi",
+	"destCode" : "DEL",
+	"depTime" : "8:20",
+	"arrTime" : "10:55",
+	"cost" : "5416"
+}, {
+	"flightId" : "SG-224",
+	"airline" : "Spicejet",
+	"source" : "Bangalore",
+	"sourceCode" : "BLR",
+	"destination" : "New Delhi",
+	"destCode" : "DEL",
+	"depTime" : "8:50",
+	"arrTime" : "11:30",
+	"cost" : "6646"
+}, {
+	"flightId" : "6E-122",
+	"airline" : "Indigo",
+	"source" : "Bangalore",
+	"sourceCode" : "BLR",
+	"destination" : "New Delhi",
+	"destCode" : "DEL",
+	"depTime" : "15:00",
+	"arrTime" : "17:40",
+	"cost" : "10356 "
+}, {
+	"flightId" : "G8-118",
+	"airline" : "Go Air",
+	"source" : "Bangalore",
+	"sourceCode" : "BLR",
+	"destination" : "New Delhi",
+	"destCode" : "DEL",
+	"depTime" : "20:15",
+	"arrTime" : "22:50",
+	"cost" : "5663"
+}, {
+	"flightId" : "SG-216",
+	"airline" : "Spicejet",
+	"source" : "Bangalore",
+	"sourceCode" : "BLR",
+	"destination" : "New Delhi",
+	"destCode" : "DEL",
+	"depTime" : "21:05",
+	"arrTime" : "23:50",
+	"cost" : "4663"
+}, {
+	"flightId" : "9W-7120",
+	"airline" : "Jet Airways",
+	"source" : "Bangalore",
+	"sourceCode" : "BLR",
+	"destination" : "New Delhi",
+	"destCode" : "DEL",
+	"depTime" : "17:35",
+	"arrTime" : "20:04",
+	"cost" : "7986"
+}, {
+	"flightId" : "AI-505",
+	"airline" : "Air India",
+	"source" : "Bangalore",
+	"sourceCode" : "BLR",
+	"destination" : "New Delhi",
+	"destCode" : "DEL",
+	"depTime" : "10:30",
+	"arrTime" : "12:55",
+	"cost" : "8533"
+}, {
+	"flightId" : "AI-503",
+	"airline" : "Air India",
+	"source" : "Bangalore",
+	"sourceCode" : "BLR",
+	"destination" : "New Delhi",
+	"destCode" : "DEL",
+	"depTime" : "16:30",
+	"arrTime" : "19:25",
+	"cost" : "6786"
+} ];
